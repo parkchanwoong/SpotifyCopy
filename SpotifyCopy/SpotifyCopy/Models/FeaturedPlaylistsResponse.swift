24dab5f798ca90c67593e0da44dda7de83267580
박찬웅 <foodi1121@gmail.com>
2021-02-27 19:14:23 +0900

    FeaturedPlaylistResponse, RecommendedGenresResponse 추가

---
+++ SpotifyCopy/SpotifyCopy/Models/FeaturedPlaylistsResponse.swift
@@ -0,0 +1,31 @@
+//
+//  FeaturedPlaylistsResponse.swift
+//  SpotifyCopy
+//
+//  Created by 박찬웅 on 2021/02/27.
+//
+
+import Foundation
+
+struct FeaturedPlaylistsResponse: Codable {
+    let playlists: PlaylistResponse
+}
+
+struct PlaylistResponse: Codable {
+    let items: [Playlist]
+}
+
+struct Playlist: Codable {
+    let description: String
+    let external_urls: [String: String]
+    let id: String
+    let images: [APIImage]
+    let name: String
+    let owner: User
+}
+
+struct User: Codable {
+    let display_name: String
+    let external_urls: [String: String]
+    let id: String
+}
---
+++ SpotifyCopy/SpotifyCopy/Models/RecommendedGenresResponse.swift
@@ -0,0 +1,12 @@
+//
+//  RecommendedGenresResponse.swift
+//  SpotifyCopy
+//
+//  Created by 박찬웅 on 2021/02/27.
+//
+
+import Foundation
+
+struct RecommendedGenresResponse: Codable {
+    let genres: [String]
+}
