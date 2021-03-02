6cf46a2a7a13c763936ade97d5e44a2673a9f0b1
박찬웅 <foodi1121@gmail.com>
2021-03-02 22:47:22 +0900

    Model 파일추가

---
+++ SpotifyCopy/SpotifyCopy/Models/RecommendationsResponse.swift
@@ -0,0 +1,18 @@
+//
+//  RecommendationsResponse.swift
+//  SpotifyCopy
+//
+//  Created by 박찬웅 on 2021/03/02.
+//
+
+import Foundation
+
+struct RecommendationsResponse: Codable {
+    let tracks: [AudioTrack]
+}
+
+
+
+
+
+
